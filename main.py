d97a85250a6ac4f5334652c1d38c02f9850d98fd
TmHive <rmush8713@gmail.com>
2026-02-14 22:31:36 +0300

    It can move

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    while True == True:
+    while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
